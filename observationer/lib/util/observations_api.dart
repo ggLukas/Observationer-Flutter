807013363f59beab691fed71e012fe5cc740a1af
Rilind Zejnullahu <58613659+RilindZejnullahu@users.noreply.github.com>
2020-11-05 19:30:24 +0100

    Optimized-loading-images
    Only load image when you press on an observation

--- observationer/lib/util/observations_api.dart
+++ observationer/lib/util/observations_api.dart
@@ -5,6 +5,7 @@ import 'package:http/http.dart' as http;
 /// This class is responsible for communication with the observations API.
 class ObservationsAPI {
   final List<Observation> observations = [];
+  final List<String> imageUrl = [];
 
   /// Fetches all observations from the database.
   ///
@@ -18,51 +19,51 @@ class ObservationsAPI {
       var jsonData = json.decode(data.body);
 
       for (int i = jsonData.length - 1; i >= 0; i--) {
-        Observation obs;
-
-        //Get the image of observation
-        var imageData = await http.get(
-            'https://saabstudent2020.azurewebsites.net/observation/' +
-                jsonData[i]['id'].toString() +
-                '/attachment');
-
-        if (imageData.statusCode == 200) {
-          var jsonImageData = json.decode(imageData.body);
-          List<String> imageUrl = [];
-
-          //If the observation has image(s)
-          if (!jsonImageData.isEmpty) {
-            for (int y = 0; y < jsonImageData.length; y++) {
-              imageUrl.add(
-                  'https://saabstudent2020.azurewebsites.net/observation/' +
-                      jsonData[i]['id'].toString() +
-                      '/attachment/' +
-                      jsonImageData[y]['id'].toString() +
-                      '/data');
-            }
-          } else {
-            //Temporary fix(probably). Right now, in one_observation_page we take the imageUrl[0], so cant be empty
-            imageUrl.add('');
-          }
-
-          obs = Observation(
-              jsonData[i]['id'],
-              jsonData[i]['subject'],
-              jsonData[i]['body'],
-              jsonData[i]['created'],
-              jsonData[i]['position']['longitude'],
-              jsonData[i]['position']['latitude'],
-              imageUrl);
-        } else {
-          throw Exception('Failed to load attachment page for an observation');
-        }
+        //If the observation has image(s)
+        Observation obs = Observation(
+            jsonData[i]['id'],
+            jsonData[i]['subject'],
+            jsonData[i]['body'],
+            jsonData[i]['created'],
+            jsonData[i]['position']['longitude'],
+            jsonData[i]['position']['latitude'],
+            ['']);
 
         observations.add(obs);
       }
-
       return observations;
     } else {
       throw Exception('Failed to load observations');
     }
+  }
+
+  Future<List<String>> fetchObservationImages(Observation observation) async {
+    Observation obs = observation;
+
+    var imageData = await http.get(
+        'https://saabstudent2020.azurewebsites.net/observation/' +
+            obs.id.toString() +
+            '/attachment');
+    if (imageData.statusCode == 200) {
+      var jsonImageData = json.decode(imageData.body);
+
+      if (!jsonImageData.isEmpty) {
+        for (int y = 0; y < jsonImageData.length; y++) {
+          imageUrl.add(
+              'https://saabstudent2020.azurewebsites.net/observation/' +
+                  obs.id.toString() +
+                  '/attachment/' +
+                  jsonImageData[y]['id'].toString() +
+                  '/data');
+        }
+      } else {
+        //Temporary fix(probably). Right now, in one_observation_page we take the imageUrl[0], so cant be empty
+        imageUrl.add('');
+      }
+    } else {
+      throw Exception('Failed to load observation image page');
+    }
+
+    return imageUrl;
   }
 }
